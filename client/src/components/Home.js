d6aa319fb7307576dae44399039fcff9934d9715
An Wang <anwang2009@gmail.com>
2019-10-31 18:20:36 -0700

    add home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Home test
+            </div>
+        );
+    }
+}
+
+Home.propTypes = {
+
+}
+
+export default Home;
